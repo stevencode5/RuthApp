f4ba2d632ae0ea3105516c030242f4b9a6aff85f
stevencode5 <stevencode50@gmail.com>
2019-01-18 15:07:40 -0500

    Creacion de campos y formulario para la creacion de producto

--- lib/pages/productos/crear-producto.dart
+++ lib/pages/productos/crear-producto.dart
@@ -1,17 +1,113 @@
 import 'package:flutter/material.dart';
+import 'package:ruthapp/pages/productos/producto.dart';
+import 'package:ruthapp/pages/productos/servicioProductos.dart';
 
-class CrearProducto extends StatelessWidget {
+class CrearProducto extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _CrearProductoState();
+  }
+}
+
+class _CrearProductoState extends State<CrearProducto> {
+
+  final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+
+  ServicioProducto servicioProducto;
+
+  Producto _nuevoProducto;
+
+  _CrearProductoState() {
+    servicioProducto = new ServicioProducto();
+    _nuevoProducto = new Producto();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Crear Producto !"),
+          title: Text("Nuevo producto"),
         ),
-        body: Container(
-          child: Center(
-            child: Text("Crear Producto !")
-          )));
+        body: _crearFormulario(context));
   }
 
+  Form _crearFormulario(BuildContext context) {
+    return Form(
+      key: _formKey,
+      autovalidate: true,
+      child: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 16.0),
+        children: <Widget>[
+          _crearTextFieldNombre(),
+          _crearTextFieldPrecio(),
+          _crearTextFieldCantidad(),
+          _crearTextFieldImagen(),
+          _crearBotonCargar(context)
+        ],
+      ),
+    );
+  }
+
+  TextFormField _crearTextFieldNombre() {
+    return TextFormField(
+      onSaved: (String nombre) { _nuevoProducto.nombre = nombre; },
+      decoration: InputDecoration(
+        icon: Icon(Icons.local_grocery_store),
+        hintText: 'Ingrese el nombre del producto',
+        labelText: 'Nombre',
+      ),
+    );
+  }
+
+  TextFormField _crearTextFieldPrecio() {
+    return TextFormField(
+      onSaved: (String precio) { _nuevoProducto.precio = int.parse(precio); },
+      keyboardType: TextInputType.numberWithOptions(),
+      decoration: InputDecoration(
+        icon: Icon(Icons.attach_money),
+        hintText: 'Ingrese el precio del producto',
+        labelText: 'Precio',
+      ),
+    );
+  }
+
+  TextFormField _crearTextFieldCantidad() {
+    return TextFormField(
+      onSaved: (String cantidad) { _nuevoProducto.cantidad = int.parse(cantidad); },
+      keyboardType: TextInputType.numberWithOptions(),
+      decoration: InputDecoration(
+        icon: Icon(Icons.format_list_numbered),
+        hintText: 'Ingrese la cantidad del producto',
+        labelText: 'Cantidad',
+      ),
+    );
+  }
+
+  TextFormField _crearTextFieldImagen() {
+    return TextFormField(
+            
+      decoration: InputDecoration(
+        icon: Icon(Icons.image),
+        hintText: 'Ingrese una imagen del producto',
+        labelText: 'Imagen',
+      ),
+    );
+  }
+
+  Container _crearBotonCargar(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.only(left: 40.0, top: 20.0),
+        child: RaisedButton(
+          color: Colors.lightBlue,
+          child: Text("Crear", style: TextStyle(color: Colors.white)),
+          //onPressed: _crearProducto(context),
+        ));
+  }
+
+  _crearProducto(BuildContext context) {
+    print("Entro a crear Producto !");
+    this.servicioProducto.crearProducto(this._nuevoProducto);
+    //Navigator.pop(context);
+  }
+  
 }
--- lib/pages/productos/servicioProductos.dart
+++ lib/pages/productos/servicioProductos.dart
@@ -1,24 +1,41 @@
 import 'package:ruthapp/pages/productos/producto.dart';
 
 class ServicioProducto {
-  List<Producto> consultarTodosProductos() {
-    List<Producto> productos = new List<Producto>();
-    productos = new List();
-    productos.add(new Producto("1", "Papas", 34, 1200, "assets/imgs/dulce.png"));
-    productos.add(new Producto("2", "Gansito", 56, 800, "assets/imgs/dulce.png"));
-    productos.add(new Producto("3", "Quimbaya", 23, 400, "assets/imgs/dulce.png"));
-    productos.add(new Producto("4", "Arepa", 56, 1500, "assets/imgs/dulce.png"));
-    productos.add(new Producto("5", "Empanada", 57, 1500, "assets/imgs/dulce.png"));
-    productos.add(new Producto("6", "Queso", 3, 1200, "assets/imgs/dulce.png"));
-    productos.add(new Producto("7", "Chocorramo", 10, 1500, "assets/imgs/dulce.png"));
-    
-    productos.add(new Producto("8", "Chicarrones", 34, 1200, "assets/imgs/dulce.png"));
-    productos.add(new Producto("9", "Tinto", 56, 800, "assets/imgs/dulce.png"));
-    productos.add(new Producto("10", "Cafe", 23, 400, "assets/imgs/dulce.png"));
-    productos.add(new Producto("11", "Piloncillo", 56, 1500, "assets/imgs/dulce.png"));
-    productos.add(new Producto("12", "Hit", 57, 1500, "assets/imgs/dulce.png"));
-    productos.add(new Producto("13", "Coca Cola", 3, 1200, "assets/imgs/dulce.png"));
-    productos.add(new Producto("14", "Limonada", 10, 1500, "assets/imgs/dulce.png"));
-    return productos;
+
+  List<Producto> _productos;
+
+  ServicioProducto(){
+    _productos = new List<Producto>();
+    // _crearProductosIniciales();
   }
+
+  void _crearProductosIniciales() {
+
+    _productos.add(Producto.fromDatos("1", "Papas", 34, 1200, "assets/imgs/dulce.png"));
+    _productos.add(Producto.fromDatos("2", "Gansito", 56, 800, "assets/imgs/dulce.png"));
+    _productos.add(Producto.fromDatos("3", "Quimbaya", 23, 400, "assets/imgs/dulce.png"));
+    _productos.add(Producto.fromDatos("4", "Arepa", 56, 1500, "assets/imgs/dulce.png"));
+    _productos.add(Producto.fromDatos("5", "Empanada", 57, 1500, "assets/imgs/dulce.png"));
+    _productos.add(Producto.fromDatos("6", "Queso", 3, 1200, "assets/imgs/dulce.png"));
+    _productos.add(Producto.fromDatos("7", "Choc7orramo", 10, 1500, "assets/imgs/dulce.png"));
+    
+    _productos.add(Producto.fromDatos("8", "Chicarrones", 34, 1200, "assets/imgs/dulce.png"));
+    _productos.add(Producto.fromDatos("9", "Tinto", 56, 800, "assets/imgs/dulce.png"));
+    _productos.add(Producto.fromDatos("10", "Cafe", 23, 400, "assets/imgs/dulce.png"));
+    _productos.add(Producto.fromDatos("11", "Piloncillo", 56, 1500, "assets/imgs/dulce.png"));
+    _productos.add(Producto.fromDatos("12", "Hit", 57, 1500, "assets/imgs/dulce.png"));
+    _productos.add(Producto.fromDatos("13", "Coca Cola", 3, 1200, "assets/imgs/dulce.png"));
+    _productos.add(Producto.fromDatos("14", "Limonada", 10, 1500, "assets/imgs/dulce.png"));
+
+  }
+
+  List<Producto> consultarTodosProductos() {
+    return this._productos;
+  }
+
+  void crearProducto(Producto producto){
+    this._productos.add(producto);
+    print("Cantidad de productos $this._productos.length");    
+  }
+
 }
